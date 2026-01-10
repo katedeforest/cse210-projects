7523d30bab0b1b23ef8430b61597c71856ad7be4
Kate DeForest <katedeforest02@gmail.com>
2026-01-09 21:19:12 -0700

    Exercise 2: If Statements

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,74 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        
+        // Ask for percentage
+        Console.Write("Please provide your percentage in the class: ");
+        int percent = int.Parse(Console.ReadLine());
+        string letter = " ";
+        string sign = " ";
+        
+        // Grade letter
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        
+        // Grade sign (+ OR -)
+        if ((percent%10) >= 7)
+        {
+            if (letter == "A" || letter == "F")
+            {
+                sign = " ";
+            }
+            else
+            {
+                sign = "+";
+            }
+        
+        }
+        else if ((percent%10) < 3)
+        {
+            if (letter == "F")
+            {
+                sign = " ";
+            }
+            else
+            {
+                sign = "-";
+            }
+        }
+        else
+        {
+            sign = " ";
+        }
+        
+        // Output
+        Console.WriteLine($"Your grade: {letter}{sign}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed the class! Congratulations!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass the class. Take it again to get a better grade!");
+        }
     }
 }
