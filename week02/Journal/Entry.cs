fee1dad51ead2ba959f7d818982193f28bfa8984
Kate DeForest <katedeforest02@gmail.com>
2026-01-15 18:32:03 -0700

    W02 Team Activity: Journal Design

---
+++ week02/Journal/Entry.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+    public void Display()
+    {}
+}
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class PromptGenerator
+{
+    public string _prompts;
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+}
