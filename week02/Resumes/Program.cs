40b58857d8c2da6a4ab284e93a123378a4330e1d
Kate DeForest <katedeforest02@gmail.com>
2026-01-15 17:32:17 -0700

    W02 Learning Activity (Resume)

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Graphic Designer";
+        job1._startYear = 2002;
+        job1._endYear = 2004;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Manager";
+        job2._startYear = 2004;
+        job2._endYear = 2010;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
