825d8c3cdf878abf56c4fbdb542a1ea734cfa7a8
Kate DeForest <katedeforest02@gmail.com>
2026-02-11 16:27:30 -0700

    W06 Learning Activity: Polymorphism

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square("Red", 5));
+        shapes.Add(new Rectangle("Blue", 10, 5));
+        shapes.Add(new Circle("Yellow", 5));
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"{shape.GetColor()}, {shape.GetArea()}");
+        }
+        
+
     }
 }
